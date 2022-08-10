6327f984a76864200a92614a0444cc6431692e3f
Miten Gajjar <miten3377@gmail.com>
2022-08-11 00:44:27 +0530

    changed .gitignore

--- src/components/Button/Button.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import * as Styles from "./Button.style.ext";
-
-const Button = () => {
-    return <Styles.Wrapper>Button</Styles.Wrapper>;
-};
-
-export default Button;
